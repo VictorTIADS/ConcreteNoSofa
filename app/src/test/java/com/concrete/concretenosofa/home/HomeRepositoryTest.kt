c47a47420f7e0de938f2ad47b597aae17f00c678
daividvleal <daivid.v.leal@gmail.com>
2020-05-05 17:34:18 -0300

    refactor repository test

--- app/src/test/java/com/concrete/concretenosofa/home/HomeRepositoryTest.kt
+++ app/src/test/java/com/concrete/concretenosofa/home/HomeRepositoryTest.kt
@@ -3,11 +3,16 @@ package com.concrete.concretenosofa.home
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.concrete.concretenosofa.mockBaseModelSuccessWeatherInfo
 import com.concrete.concretenosofa.mockWeatherSuccessResponse
+import com.concrete.concretenosofa.models.BaseModel
+import com.concrete.concretenosofa.models.WeatherRequestResponse
 import com.concrete.concretenosofa.network.RetrofitConfig
 import com.concrete.concretenosofa.repository.ServicesRepository
 import com.concrete.concretenosofa.rules.CoroutineTestRule
 import io.mockk.coEvery
 import io.mockk.mockk
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Rule
@@ -25,9 +30,13 @@ class HomeRepositoryTest {
     val service = ServicesRepository(api)
 
     @Test
-    fun givenRepositorySuccessCall_whenCallGetWeatherInfo_shouldReturnBaseModelObjectCreated() = runBlocking {
+    fun givenRepositorySuccessCall_whenCallGetWeatherInfo_shouldReturnBaseModelObjectCreated() {
         coEvery { api.retrofitRequest().getWeather() } returns mockWeatherSuccessResponse()
-        assertEquals(mockBaseModelSuccessWeatherInfo(), service.getWeatherInfo())
+        lateinit var actual: BaseModel<WeatherRequestResponse>
+        runBlocking{
+            actual = service.getWeatherInfo()
+        }
+        assertEquals(mockBaseModelSuccessWeatherInfo(), actual)
     }
 
 }
